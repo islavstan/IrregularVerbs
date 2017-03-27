184acf07d3acfedd085dd04b66d4d7989e68d390
islavstan <islavstan@gmail.com>
2017-03-27 23:14:50 +0300

    Initial commit

---
+++ app/src/main/java/com/islavstan/irregularverbs/SplashScreenActivity.java
@@ -0,0 +1,60 @@
+package com.islavstan.irregularverbs;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashScreenActivity extends AppCompatActivity  {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
+
+        //проверяет юзер залогинен или нет
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                    Log.d("stas", "signed_in:" + user.getUid());
+
+                } else {
+                    Intent intent = new Intent(SplashScreenActivity.this, RegistrationActivity.class);
+                    startActivity(intent);
+                    Log.d("stas", "signed_out");
+                }
+
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+
+}
+
